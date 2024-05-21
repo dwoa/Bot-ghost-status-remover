24c65601ea1b7761d44b63eb4a91bdf5603084c3
dwoa <tort@duck.com>
2024-05-21 16:55:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Doing raccoon things.","Going thru your trash."];
 
 
 let currentIndex = 0;
